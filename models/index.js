7518a700e6e837ef4116f682dcc76e5b10d9b596
ppreyer <parker.preyer1@gmail.com>
2017-12-20 11:04:06 -0500

    index file updated

--- models/index.js
+++ models/index.js
@@ -15,6 +15,25 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+if (sequelize) {
+    sequelize.authenticate().then(function() {
+        var config = sequelize.connectionManager.config;
+        console.log('sequelize-heroku: Connected to ' + config.host + ' as ' + config.username + '.');
+
+        sequelize.query('SELECT 1+1 as test').then(function(res) {
+
+            console.log('1+1=' + res[0].test);
+
+        });
+
+    }).catch(function(err) {
+        var config = sequelize.connectionManager.config;
+        console.log('Sequelize: Error connecting ' + config.host + ' as ' + config.user + ': ' + err);
+    });
+} else {
+    console.log('No environnement variable found.');
+}
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
